6fe79a2f79a6702c6f02c9cde7fc1af05b0ffe50
julia- <julia.ryanx@gmail.com>
2018-01-13 10:36:34 +1100

    create user model

---
+++ api/models/User.js
@@ -0,0 +1,15 @@
+const mongoose = require('./init')
+const passportLocalMongoose = require('passport-local-mongoose')
+
+const userSchema = new mongoose.Schema({
+  firstName: String,
+  lastName: String
+})
+
+userSchema.plugin(passportLocalMongoose, {
+  usernameField: 'email',
+  usernameLowerCase: true,
+  session: false
+})
+
+const User = (module.exports = mongoose.model('User', userSchema))
