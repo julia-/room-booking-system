b90923d368b422e0fa72ae8a3171c3f150845e64
julia- <julia.ryanx@gmail.com>
2018-01-13 10:38:07 +1100

    middleware for authentication and authorisation functions

---
+++ api/middleware/auth.js
@@ -0,0 +1,75 @@
+const passport = require('passport')
+const JWT = require('JWT')
+const PassportJWT = require('passport-jwt')
+const User = require('../models/User')
+
+const jwtSecret = process.env.JWT_SECRET
+const jwtAlgorithm = process.env.JWT_ALGORITHM
+const jwtExpiresIn = process.env.JWT_EXPIRES_IN
+
+passport.use(User.createStrategy())
+
+const signUp = (req, res, next) => {
+  const User = new User({
+    email: req.body.email,
+    firsName: req.body.firsName,
+    lastName: req.body.lastName
+  })
+
+  User.register(user, req.body.password, (error, user) => {
+    if (error) {
+      next(error)
+      return
+    }
+  })
+
+  req.user = user
+  next()
+}
+
+const signJWTForUser = (req, res) => {
+  const user = req.user
+  const token = JWT.sign(
+    {
+      email: user.email
+    },
+    jwtSecret,
+    {
+      algorithm: jwtAlgorithm,
+      expiresIn: jwtExpiresIn,
+      subject: user._id.toString()
+    }
+  )
+  res.json({ token })
+}
+
+passport.use(
+  new PassportJWT.Strategy(
+    {
+      jwtFromRequest: PassportJWT.ExtractJwt.fromAuthHeaderAsBearerToken(),
+      secretOrKey: jwtSecret,
+      algorithms: [jwtAlgorithm]
+    },
+    (payload, done) => {
+      User.findById(payload.sub)
+        .then(user => {
+          if (user) {
+            done(null, user)
+          } else {
+            done(null, false)
+          }
+        })
+        .catch(error => {
+          done(error, false)
+        })
+    }
+  )
+)
+
+module.exports = {
+  initialize: passport.initialize(),
+  register,
+  signIn: passport.authenticate('local', { session: false }),
+  requireJWT: passport.authenticate('jwt', { session: false }),
+  signJWTForUser
+}
