7b1d8fe4b7bef4414034982a6ac06f7c5a45592c
julia- <julia.ryanx@gmail.com>
2018-01-13 21:59:09 +1100

    move bookings into separate schema

--- api/models/Room.js
+++ api/models/Room.js
@@ -1,6 +1,16 @@
 const mongoose = require('./init')
 const Schema = mongoose.Schema
 
+const bookingSchema = new Schema({
+  _bookingId: Schema.Types.ObjectId,
+  user: { type: Schema.ObjectId, ref: 'User' },
+  bookingStart: Date,
+  bookingEnd: Date,
+  recurring: { type: Boolean, default: false },
+  businessUnit: { type: String, required: true },
+  purpose: { type: String, required: true }
+})
+
 const roomSchema = new Schema({
   name: { type: String, index: true, required: true },
   floor: { type: String, required: true },
@@ -13,17 +23,7 @@ const roomSchema = new Schema({
     opWalls: { type: Boolean, default: false },
     whiteBoard: { type: Boolean, default: false }
   },
-  bookings: [
-    {
-      _bookingId: Schema.Types.ObjectId,
-      user: { type: Schema.ObjectId, ref: 'User' },
-      bookingStart: Date,
-      bookingEnd: Date,
-      recurring: { type: Boolean, default: false },
-      businessUnit: { type: String, required: true },
-      purpose: { type: String, required: true }
-    }
-  ]
+  bookings: [bookingSchema]
 })
 
 const Room = (module.exports = mongoose.model('Room', roomSchema))
