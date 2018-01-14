2273855b2fd4b3b932c2861e8d6b9b696f89616f
Nick Sonner <nicksonner@gmail.com>
2018-01-14 22:43:36 +1100

    refactor token.js

--- web/src/api/auth.js
+++ web/src/api/auth.js
@@ -1,4 +1,4 @@
-import api from './init'
+import api, { setToken } from './init'
 import decodeJWT from 'jwt-decode'
 
 // Sends a POST request to /auth on the server, with the email & password returning the JWT
@@ -7,6 +7,7 @@ export function signIn({ email, password }) {
   return api.post('/auth', { email, password })
     .then((res) => {
       const token = res.data.token
+      setToken(token)
       const decodedToken = decodeJWT(token)
       return decodedToken
     })
--- web/src/api/init.js
+++ web/src/api/init.js
@@ -1,13 +1,21 @@
 import axios from 'axios'
+import { saveToken } from './token'
 
 // Create an axios instance
 const api = axios.create({
   baseURL: 'http://localhost:7000'
 })
 
-// Setting the Authorisation header for all future GET requests 
 export function setToken(token) {
-  api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+  // saves token to local storage
+  saveToken(token)
+  if (token) {
+    // Setting the Authorisation header for all future GET requests 
+    api.defaults.headers.common['Authorization'] = `Bearer ${token}`
+  }
+  else {
+    delete api.defaults.headers.common['Authorization']
+  }
 }
 
 export default api
