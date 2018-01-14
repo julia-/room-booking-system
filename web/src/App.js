b4a3fc34c8f51a661e23c02e2bc62bafba89e22c
Nick Sonner <nicksonner@gmail.com>
2018-01-14 20:15:32 +1100

    add SignInForm

--- web/src/App.js
+++ web/src/App.js
@@ -1,18 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import SignInForm from './components/SignInForm'
 
 class App extends Component {
+  onSignIn = ({email, password}) => {
+    console.log({email, password})
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>app</h1>
+        <SignInForm onSignIn={ this.onSignIn } />
       </div>
     );
   }
