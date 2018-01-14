faa21dc164f6025c85e7156088017b22e9d116e5
Tagma-1 <31788490+tagma-1@users.noreply.github.com>
2018-01-14 14:01:22 +1100

    Refactor with function deconstruction

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,20 +1,20 @@
 const express = require('express')
-const authMiddleware = require('../middleware/auth')
+const { signUp, signIn, signJWTForUser } = require('../middleware/auth')
 
 const router = new express.Router()
 
 // Sign up
 router.post(
   '/auth/sign-up',
-  authMiddleware.signUp,
-  authMiddleware.signJWTForUser
+  signUp,
+  signJWTForUser
 )
 
 // Sign in
 router.post(
   '/auth',
-  authMiddleware.signIn,
-  authMiddleware.signJWTForUser
+  signIn,
+  signJWTForUser
 )
 
 module.exports = router
