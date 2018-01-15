0afdb66f48c125c7fd367e6f2464d1c6c11da718
Tagma-1 <31788490+tagma-1@users.noreply.github.com>
2018-01-15 14:36:06 +1100

    Refactor functions

--- web/src/api/booking.js
+++ web/src/api/booking.js
@@ -1,25 +1,12 @@
 import moment from 'moment'
 import momentTimezone from 'moment-timezone'
 import axios from 'axios'
+import api from './init'
 
 
-// Functions to receive booking data (AEST) and convert to JS Date object
+// Function to receive booking data (AEST) and convert to JS Date object
 // Data expected in [year, month, date, hours, seconds] format
-const bookStartDate = (dataArray) => {
+const dateUTC = (dataArray) => {
   // Save as Date object in UTC
-  let startDate = moment(dataArray).toDate()
-  return startDate
+  return moment(dataArray).toDate()
 }
-
-const bookEndDate = (dataArray) => {
-  // Save as Date object in UTC
-  let endDate = moment(dataArray).toDate()
-  return endDate
-}
-
-
-
-
-
-
-
