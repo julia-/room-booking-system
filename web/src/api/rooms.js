99051cfcf14e7f49a602211be9cdc29ac948c8ec
Tagma-1 <31788490+tagma-1@users.noreply.github.com>
2018-01-15 18:33:08 +1100

    Add daily bookings function

--- web/src/api/rooms.js
+++ web/src/api/rooms.js
@@ -23,3 +23,24 @@ const durationHours = (bookingStart, bookingEnd) => {
   return difference.hours() + difference.minutes() / 60
 }
 
+// Accepts the current date in 'YYYY/MM/DD' format and an array of bookings and returns an array with required details - the start time and length of each booking in hours and details regarding who made the booking
+export function dailyBookings(currentDate, roomBookings) {
+  // Map each booking to an array
+  Object.keys(roomBookings).map((booking) => {
+    // Check if the booking is for the current date 
+    if (dateAEST(booking.bookingStart).format('YYYY/MM/DD') === currentDate) {
+      return [
+        // The hour on which the booking starts, calculated from 8AM as time = 0
+        dateAEST(booking.bookingStart).format('H') - 8,
+        // The duration of the booking in decimal format
+        durationHours(booking.bookingStart, booking.bookingEnd),
+        // Booking reference
+        booking._bookingID
+      ]
+    } else {
+      return null
+    }
+  })
+}
+
+
