3274d67212f0435c444c682566df9a24a90999fb
Tagma-1 <31788490+tagma-1@users.noreply.github.com>
2018-01-15 15:32:59 +1100

    Move time conversion function

--- web/src/api/booking.js
+++ web/src/api/booking.js
@@ -3,17 +3,11 @@ import momentTimezone from 'moment-timezone'
 import axios from 'axios'
 import api from './init'
 
-
 // Function to receive booking data (AEST) and convert to JS Date object
 // Data expected in [year, month, date, hours, seconds] format
 const dateUTC = (dataArray) => {
   // Save as Date object in UTC
   return moment(dataArray).toDate()
-}
-
-// Function to convert retrieved booking data (as a UTC JS Date object) to a Moment.js object in AEST
-const dateAEST = (date) => {
-  return momentTimezone(date).tz('Australia/Sydney')
 }
 
 // Make a room booking
--- web/src/api/rooms.js
+++ web/src/api/rooms.js
@@ -4,3 +4,8 @@ export function listRooms() {
   return api.get('/rooms')
     .then((res) => res.data)
 }
+
+// Function to convert retrieved booking data (as a UTC JS Date object) to a Moment.js object in AEST
+const dateAEST = (date) => {
+  return momentTimezone(date).tz('Australia/Sydney')
+}
