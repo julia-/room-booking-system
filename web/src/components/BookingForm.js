4b8c51045ca2f55b69c9197b24e4833364fe7bb6
Nick Sonner <nicksonner@gmail.com>
2018-01-15 18:36:21 +1100

    refactor onMakeBooking code

--- web/src/components/BookingForm.js
+++ web/src/components/BookingForm.js
@@ -10,22 +10,21 @@ function BookingForm({
     <form
     onSubmit={ (event) => {
       event.preventDefault()
+      // Data from input
       const formData = event.target.elements
       const roomId = roomData.id
+      // Year, Month, Day data
+      const year = parseInt(formData.year.value, 10)
+      const month = parseInt(formData.month.value, 10)
+      const day = parseInt(formData.day.value, 10)
       // startDate data
-      const startYear = parseInt(formData.startYear.value, 10)
-      const startMonth = parseInt(formData.startMonth.value, 10)
-      const startDay = parseInt(formData.startDay.value, 10)
       const startHour = parseInt(formData.startHour.value, 10)
       const startMinute = parseInt(formData.startMinute.value, 10)
-      const startDate = [startYear, startMonth, startDay, startHour, startMinute]
+      const startDate = [year, month, day, startHour, startMinute]
       // endDate data
-      const endYear = parseInt(formData.endYear.value, 10)
-      const endMonth = parseInt(formData.endMonth.value, 10)
-      const endDay = parseInt(formData.endDay.value, 10)
       const endHour = parseInt(formData.endHour.value, 10)
       const endMinute = parseInt(formData.endMinute.value, 10)
-      const endDate = [endYear, endMonth, endDay, endHour, endMinute]
+      const endDate = [year, month, day, endHour, endMinute]
 
       const businessUnit = formData.business.value
       const purpose = formData.purpose.value
@@ -36,60 +35,47 @@ function BookingForm({
     <div className="date-container">
       <div className="date-selector">
         <label>
-          {'Year: '}
-          <input type="number" name="startYear" />
-        </label>
-        <label>
-          {'Month: '}
-          <input type="number" name="startMonth" />
-        </label>
-        <label>
-          {'Day: '}
-          <input type="number" name="startDay" />
-        </label>
-        <label>
-          {'Hour: '}
+          {'Start Hour: '}
           <input type="number" name="startHour" />
         </label>
         <label>
-          {'Minute: '}
+          {'Start Minute: '}
           <input type="number" name="startMinute" />
         </label>
       </div>
       <div className="date-selector">
         <label>
-          {'Year: '}
-          <input type="number" name="endYear" />
-        </label>
-        <label>
-          {'Month: '}
-          <input type="number" name="endMonth" />
-        </label>
-        <label>
-          {'Day: '}
-          <input type="number" name="endDay" />
-        </label>
-        <label>
-          {'Hour: '}
+          {'End Hour: '}
           <input type="number" name="endHour" />
         </label>
         <label>
-          {'Minute: '}
+          {'End Minute: '}
           <input type="number" name="endMinute" />
         </label>
       </div>
     </div>
-
-      <label>
-        {'Business Unit: '}
-        <input type="text" name="business" />
-      </label>
-      <label>
-        {'Purpose: '}
-        <input type="text" name="purpose" />
-      </label>
-      <button>Submit</button>
-    </form>
+    <label>
+      {'Year: '}
+      <input type="number" name="year" />
+    </label>
+    <label>
+      {'Month: '}
+      <input type="number" name="month" />
+    </label>
+    <label>
+      {'Day: '}
+      <input type="number" name="day" />
+    </label>
+    <label>
+      {'Business Unit: '}
+      <input type="text" name="business" />
+    </label>
+    <label>
+      {'Purpose: '}
+      <input type="text" name="purpose" />
+    </label>
+    <button>Submit</button>
+  </form>
   )
 }
 
