9500e11dd91c1d8389bf1922a8d9a6b2f05b22f2
Tagma-1 <31788490+tagma-1@users.noreply.github.com>
2018-01-15 20:59:37 +1100

    Add booking filter for date

--- web/src/api/rooms.js
+++ web/src/api/rooms.js
@@ -12,28 +12,12 @@ const dateAEST = (date) => {
   return momentTimezone(date).tz('Australia/Sydney')
 }
 
-// Function to calculate the duration of the hours between the start and end of the booking
-const durationHours = (bookingStart, bookingEnd) => {
-  // convert the UTC Date objects to AEST Moment.js objeccts
-  let startDateLocal = dateAEST(bookingStart)
-  let endDateLocal = dateAEST(bookingEnd)
-  // calculate the duration of the difference between the two times
-  let difference = moment.duration(endDateLocal.diff(startDateLocal))
-  // return the difference in decimal format
-  return difference.hours() + difference.minutes() / 60
-}
-
-// Accepts the current date in 'YYYY/MM/DD' format and all of a room's bookings and returns an array of each booking which matches that date
+// Accepts the search date in 'YYYY/MM/DD' format and all of a room's bookings and filters the array for bookings that match the search date
 export function dailyBookings(currentDate, roomBookings) {
-  // Map each booking to an array
-  Object.keys(roomBookings).map((booking) => {
+  return roomBookings.filter(booking =>
     // Check if the booking is for the current date 
-    if (dateAEST(booking.bookingStart).format('YYYY/MM/DD') === currentDate) {
-      return booking
-    } else {
-      return null
-    }
-  })
+    dateAEST(booking.bookingStart).format('YYYY/MM/DD') === currentDate
+  )
 }
 
 
