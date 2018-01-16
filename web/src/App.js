6db2dc0caec65374f69293851e65beefe89ee195
julia- <julia.ryanx@gmail.com>
2018-01-16 13:03:48 +1100

    update state rooms and add RoomsList component

--- web/src/App.js
+++ web/src/App.js
@@ -1,22 +1,23 @@
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
 import SignInForm from './components/SignInForm'
+import RoomsList from './components/RoomsList'
 import { signIn, signOut } from './api/auth'
 import { listRooms } from './api/rooms'
 import { getDecodedToken } from './api/token'
 
 class App extends Component {
   state = {
-    decodedToken: getDecodedToken() // retrieves the token from local storage if valid, else will be null
+    decodedToken: getDecodedToken(), // retrieves the token from local storage if valid, else will be null
+    rooms: null
   }
 
   // Pass supplied email & password to the signIn function, returns the users token
-  onSignIn = ({email, password}) => {
-    signIn({ email, password })
-      .then((decodedToken) => {
-        console.log('signed in', decodedToken)
-        this.setState({ decodedToken })
-      })
+  onSignIn = ({ email, password }) => {
+    signIn({ email, password }).then(decodedToken => {
+      console.log('signed in', decodedToken)
+      this.setState({ decodedToken })
+    })
   }
 
   onSignOut = () => {
@@ -25,37 +26,38 @@ class App extends Component {
   }
 
   render() {
-    const { decodedToken } = this.state
+    const { decodedToken, rooms } = this.state
     const signedIn = !!decodedToken
 
     return (
       <div className="App">
         <h1>Red Hill Room System!</h1>
-        {
-          signedIn ? (
-            <div>
-              <h3>Signed in User: {decodedToken.email}</h3>
-              <button onClick={ this.onSignOut } >Log Out</button>
-            </div>
-          ) : (
-            <SignInForm onSignIn={ this.onSignIn } />
-          )
-        }
+        {signedIn ? (
+          <div>
+            <h3>Signed in User: {decodedToken.email}</h3>
+            <button onClick={this.onSignOut}>Log Out</button>
+            <RoomsList rooms={rooms} />
+          </div>
+        ) : (
+          <SignInForm onSignIn={this.onSignIn} />
+        )}
       </div>
-    );
+    )
   }
 
-  // When the App first renders
-  componentDidMount() {
-    // Load room data from database
+  load() {
     listRooms()
-      .then((rooms) => {
-        console.log('Room data:', rooms)
+      .then(rooms => {
+        this.setState({ rooms })
       })
-      .catch((error) => {
+      .catch(error => {
         console.error('Error loading room data', error)
       })
   }
+  // When the App first renders
+  componentDidMount() {
+    this.load()
+  }
 }
 
-export default App;
+export default App
