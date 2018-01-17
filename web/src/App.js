f833652f541f9673ad1b0bf5274f94fd16cae5a3
Nick Sonner <nicksonner@gmail.com>
2018-01-17 17:14:39 +1100

    resized BookingElement

--- web/src/App.js
+++ web/src/App.js
@@ -57,11 +57,11 @@ class App extends Component {
   }
 
   // Deletes a booking from the database and updates the React state
-  onDeleteBooking = ({ roomId, bookingId }) => {
+  onDeleteBooking = ( roomId, bookingId ) => {
     deleteBooking(roomId, bookingId)
       .then((updatedRoom) => {
         alert('Booking successfully deleted')
-        updateStateRoom(self, updatedRoom)
+        updateStateRoom(this, updatedRoom)
       })
     .catch(error => console.error( error.message ))
   }
