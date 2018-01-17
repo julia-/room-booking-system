2c76a38d0d9ce437541d31d4891332b72708d665
julia- <julia.ryanx@gmail.com>
2018-01-17 17:26:33 +1100

    add google sign button and event handler to open google sign in a new window

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,16 @@
-
-
 import React, { Component } from 'react'
 import './App.css'
 import './react-datetime.css'
 import SignInForm from './components/SignInForm'
 import RoomsList from './components/RoomsList'
 import BookingForm from './components/BookingForm'
-import { signIn, signOut } from './api/auth'
+import GoogleSignInButton from './components/GoogleSignInButton'
+import {
+  signIn,
+  signOut,
+  googleSignIn,
+  googleDidSignInWithToken
+} from './api/auth'
 import { listRooms } from './api/rooms'
 import { getDecodedToken } from './api/token'
 import { makeBooking } from './api/booking'
@@ -17,14 +21,14 @@ class App extends Component {
     roomData: null,
     currentRoom: {
       name: 'Room 1',
-      id: "5a5c0d782b191c21b1eebf52",
+      id: '5a5c0d782b191c21b1eebf52',
       floor: '8',
       capacity: 18,
       assets: {
         pcLab: true
       },
       bookings: []
-    },
+    }
   }
 
   // Pass supplied email & password to the signIn function, returns the users token
@@ -35,16 +39,30 @@ class App extends Component {
     })
   }
 
+  onBeginGoogleSignIn = () => {
+    // Begin journey through Google
+    googleSignIn()
+  }
+
+  onFinishGoogleSignIn = token => {
+    // Successfully return from journey with Google
+    const decodedToken = googleDidSignInWithToken(token)
+    this.setState({ decodedToken })
+  }
+
   onSignOut = () => {
     signOut()
     this.setState({ decodedToken: null })
   }
 
-  onMakeBooking = ({startDate, endDate, businessUnit, purpose, roomId}) => {
-    const bookingData = {startDate, endDate, businessUnit, purpose, roomId}
+  onMakeBooking = ({ startDate, endDate, businessUnit, purpose, roomId }) => {
+    const bookingData = { startDate, endDate, businessUnit, purpose, roomId }
     const existingBookings = this.state.currentRoom.bookings
     console.log('booking data:', bookingData)
-    makeBooking({startDate, endDate, businessUnit, purpose, roomId}, existingBookings)
+    makeBooking(
+      { startDate, endDate, businessUnit, purpose, roomId },
+      existingBookings
+    )
   }
 
   render() {
@@ -54,18 +72,23 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Red Hill Room System!</h1>
-        {
-          signedIn ? (
-            <div>
-              <h3>Signed in User: {decodedToken.email}</h3>
-              <button onClick={ this.onSignOut } >Log Out</button>
-              <RoomsList rooms={roomData} />
-              <BookingForm user={decodedToken.email} roomData={currentRoom} onMakeBooking={this.onMakeBooking} />
-            </div>
-          ) : (
-            <SignInForm onSignIn={ this.onSignIn } />
-          )
-        }
+        {signedIn ? (
+          <div>
+            <h3>Signed in User: {decodedToken.email}</h3>
+            <button onClick={this.onSignOut}>Log Out</button>
+            <RoomsList rooms={roomData} />
+            <BookingForm
+              user={decodedToken.email}
+              roomData={currentRoom}
+              onMakeBooking={this.onMakeBooking}
+            />
+          </div>
+        ) : (
+          <div>
+            <SignInForm onSignIn={this.onSignIn} />
+            <GoogleSignInButton onGoogleSignIn={this.onBeginGoogleSignIn} />
+          </div>
+        )}
       </div>
     )
   }
@@ -83,6 +106,12 @@ class App extends Component {
   // When the App first renders
   componentDidMount() {
     this.load()
+
+    window.authenticateCallback = this.onFinishGoogleSignIn
+  }
+
+  componentWillUnmount() {
+    delete window.authenticateCallback
   }
 }
 
