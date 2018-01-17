6d69a8c97420576fae804879087483fcdf342618
julia- <julia.ryanx@gmail.com>
2018-01-17 17:25:32 +1100

    get token from query string and close window

--- web/src/index.js
+++ web/src/index.js
@@ -1,8 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
-registerServiceWorker();
+const queryPairs = window.location.search.substring(1).split('&')
+console.log('queryPairs', queryPairs)
+// TODO: maybe use a better query parser
+if (!!queryPairs[0] && queryPairs[0].slice(0, 6) === 'token=') {
+  const token = queryPairs[0].slice(6)
+  window.opener.authenticateCallback(token)
+  window.close()
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
+registerServiceWorker()
