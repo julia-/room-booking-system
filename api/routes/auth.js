d234d8c19ebd4f015d9dc0d96560945e57f93c8e
julia- <julia.ryanx@gmail.com>
2018-01-17 17:10:21 +1100

    add google auth and calback routes

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,20 +1,28 @@
 const express = require('express')
-const { signUp, signIn, signJWTForUser } = require('../middleware/auth')
+const {
+  signUp,
+  signIn,
+  signInWithGoogle,
+  redirectByGoogle,
+  signJWTForUser,
+  googleSignJWTRedirectToApp
+} = require('../middleware/auth')
 
 const router = new express.Router()
 
 // Sign up
-router.post(
-  '/auth/sign-up',
-  signUp,
-  signJWTForUser
-)
+router.post('/auth/sign-up', signUp, signJWTForUser)
 
 // Sign in
-router.post(
-  '/auth',
-  signIn,
-  signJWTForUser
+router.post('/auth', signIn, signJWTForUser)
+
+router.get('/auth/google', signInWithGoogle)
+
+// Callback after google has authenticated the user
+router.get(
+  '/auth/google/redirect',
+  redirectByGoogle,
+  googleSignJWTRedirectToApp
 )
 
 module.exports = router
