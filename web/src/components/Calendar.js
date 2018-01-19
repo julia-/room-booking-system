1b4cf266b098030404ddf254c4383d4d31f20d0d
julia- <julia.ryanx@gmail.com>
2018-01-19 11:14:52 +1100

    remove defaultValue, add date change event handler and change utc to false

--- web/src/components/Calendar.js
+++ web/src/components/Calendar.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Datetime from 'react-datetime'
+import moment from 'moment'
 
 const Calendar = props => (
   <Datetime
     dateFormat="YYYY-MM-DD"
     timeFormat={false}
     input={false}
-    defaultValue={true}
-    utc={true}
-    // isValidDate={valid}
+    utc={false}
+    onChange={event => props.getCalendarDate(event._d)}
   />
 )
 
