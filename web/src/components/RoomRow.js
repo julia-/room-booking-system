b381c8048acb889f44d288dc52474ad97b2d81c2
julia- <julia.ryanx@gmail.com>
2018-01-19 11:25:51 +1100

    map bookings and availability for each room as a row

--- web/src/components/RoomRow.js
+++ web/src/components/RoomRow.js
@@ -1,15 +1,53 @@
 import React from 'react'
 import formatAssetName from '../helpers/rooms'
+import { dailyBookings } from '../api/rooms'
+
+const bookingArray = filteredBookings => {
+  let array = [...Array(24).keys()]
+  filteredBookings.forEach(booking => {
+    let startTime = booking.startHour
+    let duration = booking.duration
+    let bookingDuration = Math.floor(startTime + duration)
+    for (var i = startTime; i < bookingDuration; i++) {
+      array[i] = booking
+    }
+  })
+  return array
+}
+
+const rowMapper = array => {
+  let row = []
+  for (var i = 0; i < 13; i++) {
+    let bookingData = array[i + 8]
+    if (typeof bookingData == 'number') {
+      row.push(<td className="available">Available</td>)
+    } else {
+      row.push(
+        <td
+          className={`${bookingData.businessUnit
+            .replace(/ /g, '-')
+            .toLowerCase()}`}
+        >
+          {bookingData.businessUnit}
+        </td>
+      )
+    }
+  }
+  return row
+}
 
 const RoomRow = props => (
   <tr>
-    <td onClick={console.log('hi')}>{props.room.name}</td>
+    <td>{props.room.name}</td>
     <td>
       {Object.keys(props.room.assets).map(
         asset =>
-          props.room.assets[asset] && <span key={asset}>{formatAssetName(asset)}</span>
+          props.room.assets[asset] && (
+            <span key={asset}>{formatAssetName(asset)}</span>
+          )
       )}
     </td>
+    {rowMapper(bookingArray(dailyBookings(props.date, props.bookings)))}
   </tr>
 )
 
