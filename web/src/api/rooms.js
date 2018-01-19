01d229d3307d2dcf8e019a63334ff86717f63751
julia- <julia.ryanx@gmail.com>
2018-01-19 11:23:17 +1100

    remove dateAEST and update filteredBookings to use UTC

--- web/src/api/rooms.js
+++ web/src/api/rooms.js
@@ -3,21 +3,17 @@ import momentTimezone from 'moment-timezone'
 import api from './init'
 
 export function listRooms() {
-  return api.get('/rooms')
-    .then((res) => res.data)
-}
-
-// Function to convert retrieved booking data (as a UTC JS Date object) to a Moment.js object in AEST
-const dateAEST = (date) => {
-  return momentTimezone(date).tz('Australia/Sydney')
+  return api.get('/rooms').then(res => res.data)
 }
 
 // Accepts the search date in 'YYYY/MM/DD' format and all of a room's bookings and filters the array for bookings that match the search date
 export function dailyBookings(currentDate, roomBookings) {
-  return roomBookings.filter(booking =>
-    // Check if the booking is for the current date 
-    dateAEST(booking.bookingStart).format('YYYY/MM/DD') === currentDate
+  const filteredBookings = roomBookings.filter(
+    (
+      booking // Check if the booking is for the current date
+    ) =>
+      moment(booking.bookingStart).format('YYYY-MM-DD') ===
+      moment(currentDate).format('YYYY-MM-DD')
   )
+  return filteredBookings
 }
-
-
