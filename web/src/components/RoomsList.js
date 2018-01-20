8dfdfeb1d05f2bea2d55b5f4efdde8dc94710167
julia- <julia.ryanx@gmail.com>
2018-01-20 16:27:16 +1100

    update table structure and class names

--- web/src/components/RoomsList.js
+++ web/src/components/RoomsList.js
@@ -2,42 +2,73 @@ import React from 'react'
 import RoomRow from './RoomRow'
 
 const RoomsList = props => (
-  <table>
-    <thead>
-      <tr>
-        <th>Room</th>
-        <th>Assets</th>
-        <th>8am</th>
-        <th>9</th>
-        <th>10</th>
-        <th>11</th>
-        <th>12pm</th>
-        <th>1</th>
-        <th>2</th>
-        <th>3</th>
-        <th>4</th>
-        <th>5</th>
-        <th>6</th>
-        <th>7</th>
-        <th>8</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <th colSpan="15" className="table__header">
-          Floor Eight
-        </th>
-      </tr>
+  <table className="table">
+    <tr className="table__row table__row--header">
+      <th
+        scope="colgroup"
+        colSpan="15"
+        className="table__cell--header table__cell--align-left"
+      >
+        Floor Eight
+      </th>
+    </tr>
+    <tr className="table__row table__row--subheader">
+      <th scope="col" className="table__cell--header">
+        Room
+      </th>
+      <th scope="col" className="table__cell--header">
+        Assets
+      </th>
+      <th scope="col" className="table__cell--header">
+        8am
+      </th>
+      <th scope="col" className="table__cell--header">
+        9am
+      </th>
+      <th scope="col" className="table__cell--header">
+        10am
+      </th>
+      <th scope="col" className="table__cell--header">
+        11am
+      </th>
+      <th scope="col" className="table__cell--header">
+        12pm
+      </th>
+      <th scope="col" className="table__cell--header">
+        1pm
+      </th>
+      <th scope="col" className="table__cell--header">
+        2pm
+      </th>
+      <th scope="col" className="table__cell--header">
+        3pm
+      </th>
+      <th scope="col" className="table__cell--header">
+        4pm
+      </th>
+      <th scope="col" className="table__cell--header">
+        5pm
+      </th>
+      <th scope="col" className="table__cell--header">
+        6pm
+      </th>
+      <th scope="col" className="table__cell--header">
+        7pm
+      </th>
+      <th scope="col" className="table__cell--header">
+        8pm
+      </th>
+    </tr>
+    <tbody className="table__body">
       {props.rooms &&
         props.rooms
           .filter(room => {
             return room.floor === '8'
           })
-          .sort(function(a, b) {
-            const first = a.name.replace(/\d+/, '')
-            const second = b.name.replace(/\d+/, '')
-
-            if (first > second) {
+          .sort((first, second) => {
+            const firstRoom = first.name.replace(/\d+/, '')
+            const secondRoom = second.name.replace(/\d+/, '')
+            if (firstRoom > secondRoom) {
               return 1
             } else {
               return 0
@@ -52,17 +83,70 @@ const RoomsList = props => (
               onSelectRoom={props.onSelectRoom}
             />
           ))}
-
-      <tr>
-        <th colSpan="15" className="table__header">
+    </tbody>
+    <tbody>
+      <tr className="table__row table__row--header">
+        <th
+          scope="colgroup"
+          colSpan="15"
+          className="table__cell--header table__cell--align-left"
+        >
           Floor Thirteen
         </th>
       </tr>
-
+      <tr className="table__row table__row--subheader">
+        <th scope="col" className="table__cell--header table__cell--width">
+          Room
+        </th>
+        <th scope="col" className="table__cell--header table__cell--width">
+          Assets
+        </th>
+        <th scope="col" className="table__cell--header">
+          8am
+        </th>
+        <th scope="col" className="table__cell--header">
+          9am
+        </th>
+        <th scope="col" className="table__cell--header">
+          10am
+        </th>
+        <th scope="col" className="table__cell--header">
+          11am
+        </th>
+        <th scope="col" className="table__cell--header">
+          12pm
+        </th>
+        <th scope="col" className="table__cell--header">
+          1pm
+        </th>
+        <th scope="col" className="table__cell--header">
+          2pm
+        </th>
+        <th scope="col" className="table__cell--header">
+          3pm
+        </th>
+        <th scope="col" className="table__cell--header">
+          4pm
+        </th>
+        <th scope="col" className="table__cell--header">
+          5pm
+        </th>
+        <th scope="col" className="table__cell--header">
+          6pm
+        </th>
+        <th scope="col" className="table__cell--header">
+          7pm
+        </th>
+        <th scope="col" className="table__cell--header">
+          8pm
+        </th>
+      </tr>
       {props.rooms &&
         props.rooms
-          .sort(function(a, b) {
-            return a.name.match(/\d+/, '') - b.name.match(/\d+/, '')
+          .sort((first, second) => {
+            const firstRoom = first.name.match(/\d+/, '')
+            const secondRoom = second.name.match(/\d+/, '')
+            return firstRoom - secondRoom
           })
           .filter(room => {
             return room.floor === '13'
