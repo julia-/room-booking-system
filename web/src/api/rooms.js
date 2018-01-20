a4cb73a45865d4d6c50b255f873b611a9ee56b9a
julia- <julia.ryanx@gmail.com>
2018-01-20 16:24:30 +1100

    update class names and td content

--- web/src/api/rooms.js
+++ web/src/api/rooms.js
@@ -19,10 +19,8 @@ export function dailyBookings(currentDate, roomBookings) {
   return filteredBookings
 }
 
-
 // A function to take the bookings for a particular room on a given date and insert them into an array which maps each hour of that day
 export function bookingArray(filteredBookings) {
-  
   // An array from 1 to 24 representing each hour of the day
   let dayHours = [...Array(24).keys()]
 
@@ -31,31 +29,29 @@ export function bookingArray(filteredBookings) {
     let duration = booking.duration
     let finalHour = startTime + duration
 
-    // Push each booking into the relevant hour in the 24 hour array 
+    // Push each booking into the relevant hour in the 24 hour array
     // Loop from the beginning of the start hour to the end of the final hour (rounding half hours)
     for (let i = Math.floor(startTime); i < Math.ceil(finalHour); i++) {
-
       // Create a copy of the booking to customise for each hour
       let bookingData = Object.assign({}, booking)
 
       // Check if the total booking is half-hour long and begins on the half hour
       if (duration === 0.5 && startTime % 1 !== 0) {
         bookingData.secondHalfHour = true
-      // Check if the total booking is half-hour long and begins on the hour
+        // Check if the total booking is half-hour long and begins on the hour
       } else if (duration === 0.5 && startTime % 1 === 0) {
         bookingData.firstHalfHour = true
-      // If the booking is longer than half an hour
+        // If the booking is longer than half an hour
       } else {
         // Check if the booking starts on the half hour
-        if (i === Math.floor(startTime) && startTime % 1 !== 0 ) {
+        if (i === Math.floor(startTime) && startTime % 1 !== 0) {
           bookingData.secondHalfHour = true
         }
         // Check if the booking ends on the half hour
-        if (i === Math.ceil(finalHour - 1) && finalHour % 1 !== 0 ) {
+        if (i === Math.ceil(finalHour - 1) && finalHour % 1 !== 0) {
           bookingData.firstHalfHour = true
         }
       }
-
 
       // Add the booking object to the relevant hour in the 24 hour array
       dayHours[i] = bookingData
@@ -65,41 +61,31 @@ export function bookingArray(filteredBookings) {
   // Return the 24 hour array with all booking objects added to each hour they apply to
   return dayHours
 }
-  
+
 // Accept the 24 hour dayHours array as the day's booking data for a room
-export function rowMapper(dayHours){
+export function rowMapper(dayHours) {
   let tableRow = []
 
   // Loop through each hour from 8AM to 9PM (starting at 8AM = 0)
   for (var i = 0; i < 13; i++) {
-
-    // Extract the corresponding data from the 24 hour array 
+    // Extract the corresponding data from the 24 hour array
     let bookingData = dayHours[i + 8]
 
     // If the data for that hour is a number (not a booking object), there is no booking
     // Add a <td> element that indicates the time slot is available
     if (typeof bookingData == 'number') {
-      tableRow.push(
-        <td className="available">Available</td>
-      )
+      tableRow.push(<td className="available">&nbsp;</td>)
 
     // If there is a booking object, add a <td> element with custom class name to enable stlying
     } else {
-      tableRow.push(
-        <td
-        // Class name will show the business unit that made the booking, and whether the <td> element should be fully shaded, or half shaded (indicating a half-hour booking)
-          className={`${bookingData.businessUnit
+      tableRow.push(<td className={`table__cell--${bookingData.businessUnit // Class name will show the business unit that made the booking, and whether the <td> element should be fully shaded, or half shaded (indicating a half-hour booking)
             .replace(/ /g, '-')
             .toLowerCase()}
-            ${bookingData.firstHalfHour ? "first-half-hour" : '' }
-            ${bookingData.secondHalfHour ? "second-half-hour" : '' }`
-          }
-        >
-          {bookingData.businessUnit}
-        </td>
-      )
+            ${bookingData.firstHalfHour ? 'table__cell--first-half-hour' : ''}
+            ${bookingData.secondHalfHour ? 'table__cell--second-half-hour' : ''}`}>
+          &nbsp;
+        </td>)
     }
   }
   return tableRow
 }
-  
