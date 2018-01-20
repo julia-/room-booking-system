ebe6dd411411d2cd3100ddda77f96d8af657fdea
julia- <julia.ryanx@gmail.com>
2018-01-20 16:27:39 +1100

    add class names

--- web/src/components/RoomRow.js
+++ web/src/components/RoomRow.js
@@ -3,27 +3,19 @@ import formatAssetName from '../helpers/rooms'
 import { dailyBookings, bookingArray, rowMapper } from '../api/rooms'
 
 const RoomRow = props => (
-  <tr>
-    <td>{props.room.name}</td>
-    <td>
-      {
-        Object.keys(props.room.assets).map(
+  <tr className="table__row">
+    <th scope="row" className="table__cell--align-left">
+      {props.room.name}
+    </th>
+    <td className="table__data--asset">
+      {Object.keys(props.room.assets).map(
         asset =>
           props.room.assets[asset] && (
-            <span key={asset}>
-              { formatAssetName(asset) }
-            </span>
+            <span key={asset}>{formatAssetName(asset)}</span>
           )
-        )
-      }
+      )}
     </td>
-    {
-      rowMapper(
-        bookingArray(
-          dailyBookings(props.date, props.bookings)
-        )
-      )
-    }
+    {rowMapper(bookingArray(dailyBookings(props.date, props.bookings)))}
   </tr>
 )
 
