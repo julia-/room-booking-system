82d9705790f466c9fa2155109d0c4884ff8f07f5
julia- <julia.ryanx@gmail.com>
2018-01-21 14:24:00 +1100

    update imports

--- web/src/components/BookingFormTable.js
+++ web/src/components/BookingFormTable.js
@@ -1,147 +1,109 @@
 import React from 'react'
-import { dailyBookings, bookingArray, columnMapper } from '../api/rooms'
+import { columnMapper } from '../api/rooms'
+import { bookingArray, dailyBookings } from '../helpers/rooms'
 
 const BookingFormTable = props => (
-    <table>
-      <thead>
-        <tr>
-          <th>Time</th>
-          <th>Bookings</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>8am</td>
-            { 
-              columnMapper(
-                bookingArray(
-                  dailyBookings(props.date, props.roomData.bookings)
-                ), 8
-              )
-            }
-        </tr>
-        <tr>
-          <td>9am</td>
-            { 
-              columnMapper(
-                bookingArray(
-                  dailyBookings(props.date, props.roomData.bookings)
-                ), 9
-              )
-            }
-        </tr>
-        <tr>
-          <td>10am</td>
-            { 
-              columnMapper(
-                bookingArray(
-                  dailyBookings(props.date, props.roomData.bookings)
-                ), 10
-              )
-            }
-        </tr>
-        <tr>
-          <td>11am</td>
-            { 
-              columnMapper(
-                bookingArray(
-                  dailyBookings(props.date, props.roomData.bookings)
-                ), 11
-              )
-            }
-        </tr>
-        <tr>
-          <td>12pm</td>
-            { 
-              columnMapper(
-                bookingArray(
-                  dailyBookings(props.date, props.roomData.bookings)
-                ), 12
-              )
-            }
-        </tr>
-        <tr>
-          <td>1pm</td>
-            { 
-              columnMapper(
-                bookingArray(
-                  dailyBookings(props.date, props.roomData.bookings)
-                ), 13
-              )
-            }
-        </tr>
-        <tr>
-          <td>2pm</td>
-            { 
-              columnMapper(
-                bookingArray(
-                  dailyBookings(props.date, props.roomData.bookings)
-                ), 14
-              )
-            }
-        </tr>
-        <tr>
-          <td>3pm</td>
-            { 
-              columnMapper(
-                bookingArray(
-                  dailyBookings(props.date, props.roomData.bookings)
-                ), 15
-              )
-            }
-        </tr>
-        <tr>
-          <td>4pm</td>
-            { 
-              columnMapper(
-                bookingArray(
-                  dailyBookings(props.date, props.roomData.bookings)
-                ), 16
-              )
-            }
-        </tr>
-        <tr>
-          <td>5pm</td>
-            { 
-              columnMapper(
-                bookingArray(
-                  dailyBookings(props.date, props.roomData.bookings)
-                ), 17
-              )
-            }
-        </tr>
-        <tr>
-          <td>6pm</td>
-            { 
-              columnMapper(
-                bookingArray(
-                  dailyBookings(props.date, props.roomData.bookings)
-                ), 18
-              )
-            }
-        </tr>
-        <tr>
-          <td>7pm</td>
-            { 
-              columnMapper(
-                bookingArray(
-                  dailyBookings(props.date, props.roomData.bookings)
-                ), 19
-              )
-            }
-        </tr>
-        <tr>
-          <td>8pm</td>
-            { 
-              columnMapper(
-                bookingArray(
-                  dailyBookings(props.date, props.roomData.bookings)
-                ), 20
-              )
-            }
-        </tr>
-      </tbody>
-    </table>
+  <table>
+    <thead>
+      <tr>
+        <th>Time</th>
+        <th>Bookings</th>
+      </tr>
+    </thead>
+    <tbody>
+      <tr>
+        <td>8am</td>
+        {columnMapper(
+          bookingArray(dailyBookings(props.date, props.roomData.bookings)),
+          8
+        )}
+      </tr>
+      <tr>
+        <td>9am</td>
+        {columnMapper(
+          bookingArray(dailyBookings(props.date, props.roomData.bookings)),
+          9
+        )}
+      </tr>
+      <tr>
+        <td>10am</td>
+        {columnMapper(
+          bookingArray(dailyBookings(props.date, props.roomData.bookings)),
+          10
+        )}
+      </tr>
+      <tr>
+        <td>11am</td>
+        {columnMapper(
+          bookingArray(dailyBookings(props.date, props.roomData.bookings)),
+          11
+        )}
+      </tr>
+      <tr>
+        <td>12pm</td>
+        {columnMapper(
+          bookingArray(dailyBookings(props.date, props.roomData.bookings)),
+          12
+        )}
+      </tr>
+      <tr>
+        <td>1pm</td>
+        {columnMapper(
+          bookingArray(dailyBookings(props.date, props.roomData.bookings)),
+          13
+        )}
+      </tr>
+      <tr>
+        <td>2pm</td>
+        {columnMapper(
+          bookingArray(dailyBookings(props.date, props.roomData.bookings)),
+          14
+        )}
+      </tr>
+      <tr>
+        <td>3pm</td>
+        {columnMapper(
+          bookingArray(dailyBookings(props.date, props.roomData.bookings)),
+          15
+        )}
+      </tr>
+      <tr>
+        <td>4pm</td>
+        {columnMapper(
+          bookingArray(dailyBookings(props.date, props.roomData.bookings)),
+          16
+        )}
+      </tr>
+      <tr>
+        <td>5pm</td>
+        {columnMapper(
+          bookingArray(dailyBookings(props.date, props.roomData.bookings)),
+          17
+        )}
+      </tr>
+      <tr>
+        <td>6pm</td>
+        {columnMapper(
+          bookingArray(dailyBookings(props.date, props.roomData.bookings)),
+          18
+        )}
+      </tr>
+      <tr>
+        <td>7pm</td>
+        {columnMapper(
+          bookingArray(dailyBookings(props.date, props.roomData.bookings)),
+          19
+        )}
+      </tr>
+      <tr>
+        <td>8pm</td>
+        {columnMapper(
+          bookingArray(dailyBookings(props.date, props.roomData.bookings)),
+          20
+        )}
+      </tr>
+    </tbody>
+  </table>
 )
 
 export default BookingFormTable
