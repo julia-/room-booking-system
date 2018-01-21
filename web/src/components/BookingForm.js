8e060cf07c868636c89acd01b63645bbfab98753
Tagma-1 <31788490+tagma-1@users.noreply.github.com>
2018-01-21 11:22:22 +1100

    Refactor booking form function to ES6 syntax

--- web/src/components/BookingForm.js
+++ web/src/components/BookingForm.js
@@ -4,7 +4,8 @@ import Datetime from 'react-datetime'
 import moment from 'moment'
 import formatTime from '../helpers/bookingForm'
 
-function BookingForm({ onMakeBooking, user, roomData }) {
+const BookingForm = props => {
+
   const valid = function(current) {
     return current.day() !== 0
   }
@@ -16,20 +17,27 @@ function BookingForm({ onMakeBooking, user, roomData }) {
   // eg. 2018-04-12 => [2018, 4, 12]
   const handleDate = event => {
     const date = moment(event).format('Y M D')
+
+    // Update the current date in the application state
+    props.updateCalendar(moment(event)._i)
+
+    // Prepare a date array to make the booking
     dateArray = date.split(' ').map(item => parseInt(item, 10))
     dateArray[1] = dateArray[1] - 1
     return dateArray
   }
+
   var spanStyle = {
     color: 'blue'
   }
+
   return (
     <form
       onSubmit={event => {
         event.preventDefault()
         // Data from input
         const formData = event.target.elements
-        const roomId = roomData._id
+        const roomId = props.roomData._id
         // startDate data
         const startTime = formatTime(formData.startTime.value)
         const startDate = [...dateArray, ...startTime]
@@ -40,13 +48,14 @@ function BookingForm({ onMakeBooking, user, roomData }) {
         const businessUnit = formData.business.value
         const purpose = formData.purpose.value
         const description = formData.description.value
-        onMakeBooking({ startDate, endDate, businessUnit, purpose, roomId })
+        props.onMakeBooking({ startDate, endDate, businessUnit, purpose, roomId })
       }}
     >
-      <h2>{roomData.name}</h2>
+      <h2>{props.roomData.name}</h2>
       <h2>
-        Room ID: <span style={spanStyle}>{roomData._id}</span>
+        Room ID: <span style={spanStyle}>{props.roomData._id}</span>
       </h2>
+      <h2>Date: {moment(props.date).format('YYYY-MM-DD')}</h2>
       <div className="date-container">
         <div className="left-container">
           <Datetime
@@ -61,8 +70,8 @@ function BookingForm({ onMakeBooking, user, roomData }) {
 
         <div className="middle-container">
           <BookingFormTable 
-            roomData={roomData} 
-            date={ calendarDate ? calendarDate : new Date() } />
+            roomData={props.roomData} 
+            date={ props.date } />
         </div>
 
         <div className="right-container">
