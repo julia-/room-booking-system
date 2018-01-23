a5e74b5adc5e212cd2a77092cf0db820a09abdd4
julia- <julia.ryanx@gmail.com>
2018-01-24 01:07:48 +1100

    change link to view room availability

--- web/src/components/NavBar.js
+++ web/src/components/NavBar.js
@@ -9,7 +9,7 @@ function NavBar({
   return (
     <nav className="nav">
       <ul className="nav__list">
-        <li className="nav__item"><Link to="/bookings" className="nav__link">View Availability</Link></li>
+        <li className="nav__item"><Link to="/bookings" className="nav__link">View Room Availability</Link></li>
         <li className="nav__item"><Link to="/mybookings" className="nav__link">My Bookings</Link></li>
         <li className="nav__item"><a onClick={signOut} className="nav__link">Logout</a></li>
       </ul>
