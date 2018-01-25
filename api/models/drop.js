99fd1824cb88a7743ff198ca5968a45807a24c18
julia- <julia.ryanx@gmail.com>
2018-01-25 15:55:23 +1100

    add drop for User

--- api/models/drop.js
+++ api/models/drop.js
@@ -1,7 +1,14 @@
 const Room = require('./Room')
+const User = require('./User')
 
 Room.deleteMany()
   .then(() => {
     console.log('Deleted rooms')
+    process.exit()
+  })
+
+User.deleteMany()
+  .then(() => {
+    console.log('Deleted users')
     process.exit()
   })
