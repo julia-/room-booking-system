d7eb4901ca4edffe317062340e1ab318b5aca780
julia- <julia.ryanx@gmail.com>
2018-02-09 13:43:05 +1100

    Update props passed to RoomRow component for level 13

--- web/src/components/RoomsList.js
+++ web/src/components/RoomsList.js
@@ -5,11 +5,7 @@ import { levelEightSorter, levelThirteenSorter } from '../helpers/sorter'
 const RoomsList = props => (
   <table className="table">
     <tr className="table__row table__row--header">
-      <th
-        scope="colgroup"
-        colSpan="15"
-        className="table__cell--header table__cell--align-left table__cell--level"
-      >
+      <th scope="colgroup" colSpan="15" className="table__cell--header table__cell--level table__cell--align-left">
         Level Eight
       </th>
     </tr>
@@ -70,60 +66,56 @@ const RoomsList = props => (
           />
         ))}
     </tbody>
-    <tbody>
-      <tr className="table__row table__row--header">
-        <th
-          scope="colgroup"
-          colSpan="15"
-          className="table__cell--header table__cell--level table__cell--align-left"
-        >
-          Level Thirteen
-        </th>
-      </tr>
-      <tr className="table__row table__row--subheader">
-        <th scope="col" className="table__cell--header table__cell--width table__cell--align-left">
-          Room
-        </th>
-        <th scope="col" className="table__cell--header">
-          8am
-        </th>
-        <th scope="col" className="table__cell--header">
-          9am
-        </th>
-        <th scope="col" className="table__cell--header">
-          10am
-        </th>
-        <th scope="col" className="table__cell--header">
-          11am
-        </th>
-        <th scope="col" className="table__cell--header">
-          12pm
-        </th>
-        <th scope="col" className="table__cell--header">
-          1pm
-        </th>
-        <th scope="col" className="table__cell--header">
-          2pm
-        </th>
-        <th scope="col" className="table__cell--header">
-          3pm
-        </th>
-        <th scope="col" className="table__cell--header">
-          4pm
-        </th>
-        <th scope="col" className="table__cell--header">
-          5pm
-        </th>
-        <th scope="col" className="table__cell--header">
-          6pm
-        </th>
-        <th scope="col" className="table__cell--header">
-          7pm
-        </th>
-        <th scope="col" className="table__cell--header">
-          8pm
-        </th>
-      </tr>
+    <tr className="table__row table__row--header">
+      <th scope="colgroup" colSpan="15" className="table__cell--header table__cell--level table__cell--align-left">
+        Level Thirteen
+      </th>
+    </tr>
+    <tr className="table__row table__row--subheader">
+      <th scope="col" className="table__cell--header table__cell--width table__cell--align-left">
+        Room
+      </th>
+      <th scope="col" className="table__cell--header">
+        8am
+      </th>
+      <th scope="col" className="table__cell--header">
+        9am
+      </th>
+      <th scope="col" className="table__cell--header">
+        10am
+      </th>
+      <th scope="col" className="table__cell--header">
+        11am
+      </th>
+      <th scope="col" className="table__cell--header">
+        12pm
+      </th>
+      <th scope="col" className="table__cell--header">
+        1pm
+      </th>
+      <th scope="col" className="table__cell--header">
+        2pm
+      </th>
+      <th scope="col" className="table__cell--header">
+        3pm
+      </th>
+      <th scope="col" className="table__cell--header">
+        4pm
+      </th>
+      <th scope="col" className="table__cell--header">
+        5pm
+      </th>
+      <th scope="col" className="table__cell--header">
+        6pm
+      </th>
+      <th scope="col" className="table__cell--header">
+        7pm
+      </th>
+      <th scope="col" className="table__cell--header">
+        8pm
+      </th>
+    </tr>
+    <tbody className="table__body">
       {props.rooms &&
         levelThirteenSorter(props.rooms).map(room => (
           <RoomRow
@@ -131,10 +123,11 @@ const RoomsList = props => (
             room={room}
             bookings={room.bookings}
             date={props.date === null ? new Date() : props.date}
-            onSelectRoom={props.onSelectRoom}
+            onShowBooking={props.onShowBooking}
             onSetRoom={props.onSetRoom}
           />
-        ))}
+        ))
+      }
     </tbody>
   </table>
 )
